86ae2c5ddd67ead71a6cfef98186ad9aaf314701
Rafal Opacki <rafal.opacki@jit.team>
2020-03-24 23:58:28 +0100

    Add module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
 //components
 import { AppComponent } from "./app.component";
 import { TodoItemComponent } from "./todo-item/todo-item.component";
@@ -12,8 +13,8 @@ import { ConfirmModalComponent } from "./confirm-modal/confirm-modal.component";
 //services
 import { TodoService } from "src/services/todoService/todo.service";
 import { LocalStorageService } from "src/services/localStorageService/local-storage.service";
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -25,7 +26,14 @@ import { environment } from '../environments/environment';
     FormWrapperComponent,
     ConfirmModalComponent
   ],
-  imports: [BrowserModule, FormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
+  ],
   providers: [TodoService, LocalStorageService],
   bootstrap: [AppComponent]
 })
